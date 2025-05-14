f03ff893df8d03bafe0c0aa56e0ed4af3aae127c
EunNamCho <jen9581@gmail.com>
2025-05-15 01:13:19 +0900

    20250515

--- Baekjoon/1987.py
+++ Baekjoon/1987.py
@@ -1,26 +1,31 @@
 import sys
-sys.setrecursionlimit(100000)
 
-# input
-R,C = map(int,sys.stdin.readline().split())
-matrix = []
-dirs = [(1,0),(0,1),(-1,0),(0,-1)]
-answer = [0]
-for _ in range(R):
-    matrix.append(sys.stdin.readline().strip())
-visited = [0] * 26
+r, c = map(int, sys.stdin.readline().split())
+graph = []
 
-# algorithm - DFS
-def dfs(row,col):
-    visited[ord(matrix[row][col])-65] = 1
-    answer[0] = max(answer[0],sum(visited))
-    for dir in dirs:
-        dx,dy = dir
-        new_row, new_col = row+dy, col+dx
-        if 0<=new_row<R and 0<=new_col<C and not visited[ord(matrix[new_row][new_col])-65]:
-            dfs(new_row,new_col)
-    visited[ord(matrix[row][col])-65] = 0
-    
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
 
-dfs(0,0)
-print(answer[0])
+for _ in range(r):
+    graph.append(list(sys.stdin.readline().strip()))
+
+def dfs(sx,sy):
+    q = set()
+    q.add((sx,sx,graph[sx][sy]))
+    squares = 0
+
+    while q:
+        x, y, now_visited = q.pop()
+
+        squares = max(squares, len(now_visited))
+        if squares == 26:
+            return 26
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if (0 <= nx < r) and (0 <= ny < c) and graph[nx][ny] not in now_visited:
+                q.add((nx, ny, now_visited + graph[nx][ny]))
+    return squares
+print(dfs(0,0))
+
